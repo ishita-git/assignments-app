b8bc549865198ddf953423ea1330db42e8a3f7b3
Ishita Amod <ishitaamod@gmail.com>
2023-08-05 00:18:51 +0530

    first page completed

--- src/components/OldAssessment/OldAssessment.js
+++ src/components/OldAssessment/OldAssessment.js
@@ -2,33 +2,54 @@ import React from 'react'
 import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import InsertLinkIcon from '@mui/icons-material/InsertLink';
 import './oldassessment.scss'
 
 const OldAssessment = () => {
-  return (
-    <div className='old-assessment'>
-        <div className='top'>
-            <div className='main-container'>
-                <div className='icon-container'><BusinessCenterOutlinedIcon className='icon'/></div>
-                <div className='text-container'>
-                    <div className='label'>Math Assessment</div>
-                    <div className='small-text'>
-                        <div className='job'>Job</div>
-                        <hr/>
-                        <div className='date-container'>< CalendarMonthOutlinedIcon/>20 April 2023</div>
-                    </div>
+    return (
+        <div className='old-assessment'>
+            <div className='top'>
+                <div className='main-container'>
+                    <div className='icon-container'><BusinessCenterOutlinedIcon className='icon' /></div>
+                    <div className='text-container'>
+                        <div className='label'>Math Assessment</div>
+                        <div className='small-text'>
+                            <div className='job'>Job</div>
+                            <hr />
+                            <div className='date-container'>< CalendarMonthOutlinedIcon />20 April 2023</div>
+                        </div>
 
+                    </div>
+                </div>
+                <div className='three-dots'><MoreVertOutlinedIcon /></div>
+            </div>
+            <hr />
+            <div className='bottom'>
+                <div className='left'>
+                    <div className='duration'>
+                        <div className='number'>00</div>
+                        <div className='label'>Duration</div>
+
+                    </div>
+                    <div className='questions'>
+                        <div className='number'>00</div>
+                        <div className='label'>Questions</div>
+
+                    </div>
+                </div>
+                <div className='right'>
+                    <div className='share-container'>
+                        <InsertLinkIcon className='icon'/>
+                        <div className='label'>Share</div>
+
+                    </div>
+                    <div className='user-container'>
+                        <div className='text'>LP</div>
+                    </div>
                 </div>
             </div>
-            <div className='three-dots'><MoreVertOutlinedIcon/></div>
         </div>
-        <hr/>
-        <div className='bottom'>
-            <div className='left'>left</div>
-            <div className='right'>right</div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default OldAssessment
