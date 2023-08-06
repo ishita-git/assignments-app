8cc89d44378a12735e9b2d66b7074a6c29e51220
Ishita Amod <ishitaamod@gmail.com>
2023-08-06 23:10:51 +0530

    onclick

--- src/components/NewAssessment/NewAssessment.js
+++ src/components/NewAssessment/NewAssessment.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import './newassessment.scss'
 
 const NewAssessment = () => {
+
+  const[click,setClick] = useState(false);
+
+  const onClickHandler = () =>{
+    setClick(true);
+  }
+
   return (
     <div className='new-assessment'>
 
         <div className='upload-container'>
-          <div className='icon-container'><AddOutlinedIcon className='icon'/></div>
+          <div className='icon-container'><AddOutlinedIcon className='icon' onClick={onClickHandler}/></div>
           <div className='label'>New Assessment</div>
           <div className='content'>From here you can add questions of multiple types like MCQs, subjective (text or paragraph)!</div>
         </div>
